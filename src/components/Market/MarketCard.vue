5150df894222bf907d3a60be5a7da11ef93289d0
vlasapros <vlasapros@gmail.com>
2020-08-26 01:16:47 +0700

    Начал переделывать Market

--- src/components/Market/MarketCard.vue
+++ src/components/Market/MarketCard.vue
@@ -1,7 +1,8 @@
 <template lang="html">
     <div class='card'>
-        <img :src="pic" width="236" height="128">
-        <h1>{{ title }}</h1>
+        <div class='picture' v-bind:style="{backgroundImage:pic}"></div>
+        <!-- <img class='pic' :src="pic"> -->
+        <h1 class='title'>{{ title }}</h1>
         <h2>Стоимость: {{ cost }} монет</h2>
         <button 
             :disabled='amountCoins < cost'
@@ -32,7 +33,8 @@
 </script>
 
 <style lang="scss" scoped>
-    h1 {
+
+    .title {
         font-family: 'Montserrat_regular';
         font-style: normal;
         font-weight: 600;
@@ -40,7 +42,10 @@
         line-height: 32px;
         text-align: center;
         color: #FFFFFF;
+        width: 236px;
+        height: 32px;
     }
+
     h2 {
         font-family: 'Montserrat_regular';
         font-style: normal;
@@ -50,12 +55,7 @@
         text-align: center;
         color: #A3B8CC;
     }
-    .card {
-        display: flex;
-        flex-flow: column;
-        justify-content: center;
-        align-items: center;
-    }
+
     .button {
         width: 200px;
         height: 48px;
@@ -70,10 +70,51 @@
         color: #FFFFFF;
         border: none;
     }
+
     .active {
         background: linear-gradient(180deg, #FF7F22 0%, #FF5722 100%);
     }
+
+    .button:hover {
+        background: linear-gradient(180deg, #FF5722 0%, #FF5722 100%);
+    }
+
+    .button:active {
+        background: linear-gradient(180deg, #FF7F22 0%, #FF7F22 100%);
+    }
+
     .button:disabled {
         color: #7D90A6;
     }
+
+    .card {
+        position: relative;
+        display: flex;
+        flex-flow: column;
+        justify-content: space-between;
+        align-items: center;
+        flex-direction: column;
+        height: 286px;
+        width: 200px;
+        padding-top: 153px;
+    }
+
+    .pic {
+        top:-60px;
+        left: -40.5px;
+        position:absolute;
+        width: 281px;
+        height: 247px;
+    }
+
+    .picture {
+        position: absolute;
+        left:-40px;
+        top:-79px;
+        width: 286px;
+        height: 286px;
+        background-position: center;
+    }
+
 </style>
+
